5d376e183390f3be7d951213a7e9cb7da2f9ced8
emm <723125@gmail.com>
2020-12-21 14:17:58 +0000

    Initial Commit

---
+++ CircularMusicPlayer_V1.0/CircularMusicPlayer_V1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  CircularMusicPlayer_V1_0App.swift
+//  CircularMusicPlayer_V1.0
+//
+//  Created by emm on 21/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct CircularMusicPlayer_V1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircularMusicPlayer_V1.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CircularMusicPlayer_V1.0
+//
+//  Created by emm on 21/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
